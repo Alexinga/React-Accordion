84b93fe1eee4dbe0f3b3972d13d9b65e5e766938
Alex Inga <alexinga98@gmail.com>
2023-08-24 20:12:06 -0400

    accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
+function Accordion({ data }) {
+  const [currOpen, setCurrOpen] = useState(null);
+  return (
+    <div className="accordion">
+      {data.map((items, i) => (
+        <AccordionItem
+          currOpen={currOpen}
+          title={items.title}
+          num={i + 1}
+          onOpen={setCurrOpen}
+        >
+          {items.text}
+        </AccordionItem>
+      ))}
+      <AccordionItem
+        title="Learn React"
+        num="43"
+        currOpen={currOpen}
+        onOpen={setCurrOpen}
+      >
+        <p>Currently learning how children communicate to each other</p>
+      </AccordionItem>
+    </div>
+  );
+}
+function AccordionItem({ title, num, onOpen, currOpen, children }) {
+  const isOpen = num === currOpen;
+  function handleOpenClick() {
+    onOpen(isOpen ? null : num);
+  }
+  return (
+    <div className={isOpen ? "item open" : "item"} onClick={handleOpenClick}>
+      <p className="number">0{num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{children}</div>}
+    </div>
+  );
+}
 export default App;
